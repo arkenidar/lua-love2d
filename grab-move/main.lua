3e0be2db21c0d01908ad707e7452987c3774783c
Dario Cangialosi Arkenidar <dario.cangialosi@gmail.com>
2021-08-07 14:35:33 +0200

    bugfix for case: grabbing external area (moved the "in-rectangle" check properly)

--- grab-move/main.lua
+++ grab-move/main.lua
@@ -8,10 +8,10 @@ function love.draw()
   love.graphics.setColor(0,0,1) -- blue color
   
   mouse={love.mouse.getX(),love.mouse.getY()}
-  if love.mouse.isDown(1) then
-    love.graphics.setColor(1,0,0) -- red color
-    if mouse_grab_offset==nil and
+  if love.mouse.isDown(1) and
         point_in_rectangle(mouse,xywh) then
+    love.graphics.setColor(1,0,0) -- red color
+    if mouse_grab_offset==nil then
       mouse_grab_offset={mouse[1]-xywh[1],mouse[2]-xywh[2]}
     else
       xywh[1]=mouse[1]-mouse_grab_offset[1]
@@ -21,10 +21,10 @@ function love.draw()
     mouse_grab_offset=nil
   end
   
-  love.graphics.rectangle("fill", xywh[1], xywh[2], xywh[3], xywh[4]) -- xywh
+  love.graphics.rectangle("fill", xywh[1], xywh[2], xywh[3], xywh[4]) -- xywh (handle)
 
   love.graphics.setColor(1,1,1) -- white color
-  love.graphics.rectangle("fill", xywh[1], xywh[2]+xywh[4], xywh[3], 4*xywh[4]) -- xywh
+  love.graphics.rectangle("fill", xywh[1], xywh[2]+xywh[4], xywh[3], 4*xywh[4]) -- xywh (area)
 end
 
 function point_in_rectangle(point,xywh)
