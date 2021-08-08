d7504383044da590d91138719766c3d7eba8c60a
Dario Cangialosi Arkenidar <dario.cangialosi@gmail.com>
2021-08-08 17:45:16 +0200

    fix: layered (separate input and drawing order). only one can grabbed at a time.

--- LOVE-files/grab-move-01-basic/main.lua
+++ LOVE-files/grab-move-01-basic/main.lua
@@ -1,34 +1,59 @@
 
 xywh1={50,50,100,100}
-xywh2={150,50,100,100}
+xywh2={150+10,50,100,100}
+handles={xywh1,xywh2,{250+20,50,100,100}}
 
-function draw_handle(xywh)
-  
-  love.graphics.setColor(1,1,1) -- white color
-  
-  local mouse={love.mouse.getX(),love.mouse.getY()}
+function mouse_coordinates()
+  return {love.mouse.getX(),love.mouse.getY()}
+end
+
+handle_currently_grabbed=nil
+function handle_grab(handle)
+  local xywh=handle
+  local mouse=mouse_coordinates()
   if love.mouse.isDown(1) then -- mouse down
     if xywh.mouse_grab_offset==nil then -- if ungrabbed
-      if point_in_rectangle(mouse,xywh) then -- if inside
+      if point_in_rectangle(mouse,xywh) -- if inside
+      and handle_currently_grabbed==nil -- if none is grabbed already
+      then
         -- grab
+        handle_currently_grabbed=handle
         xywh.mouse_grab_offset={mouse[1]-xywh[1],mouse[2]-xywh[2]}
       end
     end
     if xywh.mouse_grab_offset~=nil then -- if grabbed
-      love.graphics.setColor(1,0,0) -- red color
       xywh[1]=mouse[1]-xywh.mouse_grab_offset[1]
       xywh[2]=mouse[2]-xywh.mouse_grab_offset[2]
     end
   else -- mouse up
     xywh.mouse_grab_offset=nil -- un-grab
+    handle_currently_grabbed=nil
   end
-  
+
+end
+
+function handle_draw(handle)
+  local xywh=handle
+  love.graphics.setColor(1,1,1) -- white color
+  if xywh.mouse_grab_offset~=nil then -- if grabbed
+    love.graphics.setColor(1,0,0) -- red color
+  end
+
   love.graphics.rectangle("fill", xywh[1], xywh[2], xywh[3], xywh[4]) -- xywh
 end
 
 function love.draw()
-  draw_handle(xywh1)
-  draw_handle(xywh2)
+
+  -- input: front to back order
+  for i = #handles,1,-1 do
+    handle_grab(handles[i])
+  end
+
+  -- drawing: back to front order
+  for i = 1,#handles,1 do
+    handle_draw(handles[i])
+  end
+
 end
 
 function point_in_rectangle(point,xywh)
