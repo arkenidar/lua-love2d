5775d8c128e0a62cf6dbfe0c3b8d30f18e064eb2
Dario Cangialosi Arkenidar <dario.cangialosi@gmail.com>
2021-08-08 14:32:01 +0200

    retro-bugfix, multiple 'handle' items, better explanation in sourcecode comments

--- LOVE-files/grab-move-01-basic/main.lua
+++ LOVE-files/grab-move-01-basic/main.lua
@@ -1,27 +1,34 @@
 
-xywh={50,50,100,100}
+xywh1={50,50,100,100}
+xywh2={150,50,100,100}
 
-mouse_grab_offset=nil
-
-function love.draw()
+function draw_handle(xywh)
   
   love.graphics.setColor(1,1,1) -- white color
   
-  mouse={love.mouse.getX(),love.mouse.getY()}
-  if love.mouse.isDown(1) then
-    love.graphics.setColor(1,0,0) -- red color
-    if mouse_grab_offset==nil and
-        point_in_rectangle(mouse,xywh) then
-      mouse_grab_offset={mouse[1]-xywh[1],mouse[2]-xywh[2]}
-    else
-      xywh[1]=mouse[1]-mouse_grab_offset[1]
-      xywh[2]=mouse[2]-mouse_grab_offset[2]
+  local mouse={love.mouse.getX(),love.mouse.getY()}
+  if love.mouse.isDown(1) then -- mouse down
+    if xywh.mouse_grab_offset==nil then -- if ungrabbed
+      if point_in_rectangle(mouse,xywh) then -- if inside
+        -- grab
+        xywh.mouse_grab_offset={mouse[1]-xywh[1],mouse[2]-xywh[2]}
+      end
     end
-  else
-    mouse_grab_offset=nil
+    if xywh.mouse_grab_offset~=nil then -- if grabbed
+      love.graphics.setColor(1,0,0) -- red color
+      xywh[1]=mouse[1]-xywh.mouse_grab_offset[1]
+      xywh[2]=mouse[2]-xywh.mouse_grab_offset[2]
+    end
+  else -- mouse up
+    xywh.mouse_grab_offset=nil -- un-grab
   end
   
   love.graphics.rectangle("fill", xywh[1], xywh[2], xywh[3], xywh[4]) -- xywh
+end
+
+function love.draw()
+  draw_handle(xywh1)
+  draw_handle(xywh2)
 end
 
 function point_in_rectangle(point,xywh)
