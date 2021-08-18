c68bebeb94fcae7257045329f827180ae284dbdd
Dario Cangialosi Arkenidar <dario.cangialosi@gmail.com>
2021-08-18 09:11:01 +0200

    new: "toggle" buttons can have text labels in shown way (clicking on the label triggers the linked button's action). new: calculations of xywh also with references. new: drawing some text.

--- basic-button/conf-buttons.lua
+++ basic-button/conf-buttons.lua
@@ -36,8 +36,50 @@ function toggle_draw(button)
   image_draw(images[state],button)
 end
 function toggle_action(button) button.state=not button.state end
-button_list.toggle_1={10+100+10,10, 50,50, draw=toggle_draw, action=toggle_action, state=true}
-button_list.toggle_2={10+100+10,10+50+10, 50,50, draw=toggle_draw, action=toggle_action, state=false}
+
+function draw_centered_text(rectX, rectY, rectWidth, rectHeight, text)
+	local font       = love.graphics.getFont()
+	local textWidth  = font:getWidth(text)
+	local textHeight = font:getHeight()
+	love.graphics.print(text, rectX+rectWidth/2, rectY+rectHeight/2, 0, 1, 1, textWidth/2, textHeight/2)
+end
+function toggle_label_draw(button)
+  local xywh=button
+  --[[
+  local r,g,b=0,1,0
+  love.graphics.setColor(0.2,0.2,0.2) -- grey color (border color)
+  -- xywh (area border)
+  love.graphics.rectangle("fill", xywh[1], xywh[2], xywh[3], xywh[4])
+
+  love.graphics.rectangle("fill", xywh[1], xywh[2], xywh[3], xywh[4]) -- xywh
+  local border=5
+  love.graphics.setColor(r,g,b) -- selected color (inner color)
+  -- xywh (inner area inside the border)
+  love.graphics.rectangle("fill", xywh[1]+border, xywh[2]+border, xywh[3]-border*2, xywh[4]-border*2)
+  -- reset
+  love.graphics.setColor(1,1,1) -- reset to white
+  --]]
+  --love.graphics.print("text",xywh[1],xywh[2])
+  draw_centered_text(xywh[1],xywh[2],xywh[3],xywh[4], button.text_label)
+end
+function toggle_label_action(button)
+  -- clicking on the label triggers the linked button's action
+  button_list[button.linked]:action()
+end
+
+space=10
+size=50
+toggle_1={space+100+space,space+size+space, size,size, draw=toggle_draw, action=toggle_action, state=true}
+toggle_2={toggle_1[1],toggle_1[2]+toggle_1[4]+space, size,size, draw=toggle_draw, action=toggle_action, state=false}
+
+toggle_1_label={toggle_1[1]+toggle_1[3],toggle_1[2], size*3,size, draw=toggle_label_draw, action=toggle_label_action, linked="toggle_1", text_label="toggle 1 text label"}
+
+toggle_2_label={toggle_2[1]+toggle_2[3],toggle_2[2], size*3,size, draw=toggle_label_draw, action=toggle_label_action, linked="toggle_2", text_label="toggle 2 text label"}
+
+button_list.toggle_1=toggle_1
+button_list.toggle_2=toggle_2
+button_list.toggle_1_label=toggle_1_label
+button_list.toggle_2_label=toggle_2_label
 
 ---[[
 -- exclusive selection
